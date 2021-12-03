9595ecd69c7dab34612a3620237cdfdd4e7bd3ab
DeungHeok Chung <richard0326@naver.com>
2021-12-03 18:11:28 +0900

    간단한 login test
    - http 간격 중요함.

--- APIServer/Controllers/LoginController.cs
+++ APIServer/Controllers/LoginController.cs
@@ -12,34 +12,40 @@ namespace ApiServer.Controllers
     [Route("[controller]")]
     public class LoginController : ControllerBase
     {
-        [HttpPut]
-        public async Task<PkLoginResponse> Put(PkLoginRequest request)
+        public async Task<PkLoginResponse> Get(PkLoginRequest request)
         {
+            Console.WriteLine(request.ID);
+            Console.WriteLine(request.PW);
+            
             // 반환할 응답 객체
             var response = new PkLoginResponse();
-            response.Result = SErrorCode.None;
+            //response.Result = SErrorCode.None;
+            response.Result = 0;
             
             // 암호화된 PW를 가져온다.
             // saltstring + SHA 암호화하여 hash 값을 얻는다.
             var saltValue = ServerCommon.Security.SaltString();
             var hashingPassword = ServerCommon.Security.MakeHashingPassWord(saltValue, request.PW);
-            
+
+            string ez = request.ID;
             // DB에 확인
             if (false)
             {
-                response.Result = SErrorCode.Login_Fail_NotUser;
+                //response.Result = SErrorCode.login_Fail_NotUser;
                 return response;
             }
             
             if (false)
             {
-                response.Result = SErrorCode.Login_Fail_PW;
+                //response.Result = SErrorCode.login_Fail_PW;
                 return response;
             }
 
             // 토큰 발행
             response.Authtoken = ServerCommon.Security.AuthToken();
 
+            Console.WriteLine(response.Authtoken);
+            
             return response;
         }
     }
@@ -47,14 +53,15 @@ namespace ApiServer.Controllers
 
     public class PkLoginRequest
     {
-        public string ID;
-        public string PW;
+        public string ID { get; set; }
+        public string PW { get; set; }
     }
     
     public class PkLoginResponse
     {
-        public ServerCommon.SErrorCode Result;
-        public string Authtoken;
+        //public ServerCommon.SErrorCode Result;
+        public int Result { get; set; }
+        public string Authtoken { get; set; }
     }
 
     class DBUserInfo
--- ServerCommon/SErrorCode.cs
+++ ServerCommon/SErrorCode.cs
@@ -19,7 +19,7 @@
         // Raid Server  20601 ~ 20800
 
         // DB  21501 ~ 21800
-        Login_Fail_NotUser = 21501,
-        Login_Fail_PW = 21502,
+        login_Fail_NotUser = 21501,
+        login_Fail_PW = 21502,
     }
 }
