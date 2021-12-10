80f5946e779bb60ec6d4c1f7146dc77e5d3c3208
DeungHeok Chung <richard0326@naver.com>
2021-12-10 20:21:00 +0900

    github 버그 제거
    github 버그 제거

--- ApiServer/Model/FieldMonsterRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiServer.Model
-{
-        public class FieldMonsterRequest
-        {
-                public string ID { get; set; } = "";
-                public string AuthToken { get; set; } = "";
-        }
-}
