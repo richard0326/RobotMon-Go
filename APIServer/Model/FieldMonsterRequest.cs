2bbfb009daae1861daefce15853255e7c6885947
DeungHeok Chung <richard0326@naver.com>
2021-12-10 20:23:01 +0900

    깃허브 버그 제거
    깃허브 버그 제거

---
+++ APIServer/Model/FieldMonsterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiServer.Model
+{
+        public class FieldMonsterRequest
+        {
+                public string ID { get; set; } = "";
+                public string AuthToken { get; set; } = "";
+        }
+}
