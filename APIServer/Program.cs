ccde508ff673ff1a93e44741dcbd2c011e031885
BuildTools <richard0326@naver.com>
2022-01-20 12:23:38 +0900

    startpup 6로 변경, conf파일 추가

--- APIServer/Program.cs
+++ APIServer/Program.cs
@@ -1,70 +1,77 @@
-
-using System;
+using ApiServer;
+using ApiServer.Options;
 using ApiServer.Services;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using ZLogger;
 
-namespace ApiServer
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-            //try
-            //{
-            //    logger.Debug("init main");
-            //    CreateHostBuilder(args).Build().Run();
-            //}
-            //catch (Exception exception)
-            //{
-            //    //NLog: catch setup errors
-            //    logger.Error(exception, "Stopped program because of exception");
-            //    throw;
-            //}
-            //finally
-            //{
-            //    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-            //    NLog.LogManager.Shutdown();
-            //}
-        }
-        
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var builder = Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, configurationBuilder) =>
-                {
-                    /*
-                    // Development, Staging, Production 중 하나로 설정합시다.
-                    아직 테스트 중.
-                    // launchSettings : "ASPNETCORE_ENVIRONMENT": "Development" 설정 값이
-                    // context.HostingEnvironment.EnvironmentName의 값이 된다.
-                    configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
-                    configurationBuilder.AddJsonFile("appsettings.json");
-                    configurationBuilder.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
-                    */
-                })
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Debug); // appsettings.json 파일에 의해서 Information으로 오버라이드됨.
-                    //logging.AddZLoggerFile("logs/apiserver.log");
-                    logging.AddZLoggerConsole();                    
-                    //logging.AddZLoggerConsole(options =>
-                    //{
-                    //    // 구조화된 형식으로 로그 출력
-                    //    options.EnableStructuredLogging = true;
-                    //}); // Zlogger 사용
-                    
-                }).ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+var builder = WebApplication.CreateBuilder(args);
 
-            return builder;
-        }
+// 내가 만든 Config 파일 가져오기
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.SetBasePath(Directory.GetCurrentDirectory());
+    config.AddJsonFile("MyConfig.json",
+                       optional: true,
+                       reloadOnChange: true);
+
+    if (args != null)
+    {
+        config.AddCommandLine(args);
     }
+});
+
+// Config 파일 가져오기
+IConfiguration Configuration = builder.Configuration;
+
+// 환경 세팅하기
+if (Configuration["Environment"] == "Production")
+{
+    builder.Environment.EnvironmentName = Environments.Production;
 }
+else
+{
+    builder.Environment.EnvironmentName = Environments.Development;
+}
+
+// Config 파일 추가하기
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+
+// Config 파일 추가 등록하기
+builder.Services.Configure<DbConfig>(Configuration.GetSection(nameof(DbConfig)));
+
+// 서비스 등록
+builder.Services.AddTransient<IAccountDb, AccountDb>();
+builder.Services.AddTransient<IGameDb, GameDb>();
+builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// Zlogger 추가
+builder.Logging.ClearProviders();
+builder.Logging.AddZLoggerConsole();
+
+// app build
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    Console.WriteLine("is Develope Mode");
+}
+
+// 미들웨어 추가
+app.UseMiddleware<AuthTokenCheckMiddleware>();
+app.UseRouting();
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+// 사용자 초기화
+RedisDB.Init(Configuration["SessionConfig:SessionCacheRedisIp"]);
+DataStorage.Load(Configuration["DbConfig:GameConnStr"]);
+RankManager.Init(Configuration["DbConfig:GameConnStr"]);
+
+// 현재 모드 확인하기
+Console.WriteLine($"Program env Mode : {app.Environment.EnvironmentName}");
+Console.WriteLine($"My appsetting Mode : {Configuration["Mode"]}");
+
+// 앱 실행 - IP Port 설정
+app.Run(Configuration["IPPort"]);
